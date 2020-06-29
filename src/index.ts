bf442e248c7aa649a26ac8b34ab786066748e849
pfzheng <pengfeizheng@hotmail.com>
2020-06-29 18:49:53 +0800

    feat: 发布补丁0.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
+/*
+ * @Author: pfzhengd
+ * @Date: 2020-06-29 17:59:04
+ * @LastEditors: pfzhengd
+ * @LastEditTime: 2020-06-29 18:42:33
+ * @Description: file content
+ */ 
+
 const version:string = '0.0.1'
+
+export * from './debug'
 
 export default ():string=>{
     return `The current version is ${version}`
