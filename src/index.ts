8261b8f7b453c947cf8bb183ac8a8c5621a87703
pfzheng <pengfeizheng@hotmail.com>
2020-06-30 14:00:26 +0800

    feat: 增加了常用的函数

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,160 @@
  * @Author: pfzhengd
  * @Date: 2020-06-29 17:59:04
  * @LastEditors: pfzhengd
- * @LastEditTime: 2020-06-29 18:42:33
+ * @LastEditTime: 2020-06-30 11:58:08
  * @Description: file content
  */ 
 
-const version:string = '0.0.1'
+const hasOwnProperty = Object.prototype.hasOwnProperty
+const _toString = Object.prototype.toString
 
 export * from './debug'
 
-export default ():string=>{
-    return `The current version is ${version}`
+export const emptyObject:Readonly<{}> = Object.freeze({})
+
+/**
+ * 执行一个空操作
+ *
+ * @export
+ * @param {any} args
+ */
+export function noop (...rest:[any]):void {}
+
+/**
+ *一个精确添加的方法，思路参考:https://github.com/camsong/blog/issues/9
+ *
+ * @export
+ * @param {number} [num1=0]
+ * @param {number} [num2=0]
+ * @returns
+ */
+export function preciseAddition (num1:number = 0, num2:number = 0):number {
+  const num1Mantissa = (num1.toString().split('.')[1] || '').length
+  const num2Mantissa = (num1.toString().split('.')[1] || '').length
+  const baseNum = Math.pow(10, Math.max(num1Mantissa, num2Mantissa))
+  return (num1 * baseNum + num2 * baseNum) / baseNum
+}
+
+/**
+ *一个精确减法的方法，思路参考:https://github.com/camsong/blog/issues/9
+ *
+ * @export
+ * @param {number} [num1=0]
+ * @param {number} [num2=0]
+ * @returns
+ */
+export function preciseReduce (num1:number = 0, num2:number = 0):number {
+  const num1Mantissa = (num1.toString().split('.')[1] || '').length
+  const num2Mantissa = (num1.toString().split('.')[1] || '').length
+  const baseNum = Math.pow(10, Math.max(num1Mantissa, num2Mantissa))
+  return (num1 * baseNum - num2 * baseNum) / baseNum
+}
+
+/**
+ * 判断对象是否是纯粹的对象类型
+ *
+ * @export
+ * @param {*} obj
+ * @returns
+ */
+export function isPlainObject (obj:object):boolean{
+  if (_toString.call(obj) === '[object Object]') {
+    return true
+  }
+  return false
+}
+
+/**
+ *  检测对象里是否包含有指定属性
+ *
+ * @export
+ * @param {Object} obj
+ * @param {String} key
+ * @returns Boolean
+ */
+export function hasOwn (obj:object, key:string|number|symbol):boolean {
+  return hasOwnProperty.call(obj, key)
+}
+
+/**
+ *  混合对象里的属性到指定的对象上
+ *
+ * @export
+ * @param {Object} to
+ * @param {Object} _from
+ * @returns Object
+ */
+export function extend (to:object, _from:object):object {
+  for (const key in _from) {
+    to[key] = _from[key]
+  }
+  return to
+}
+
+/**
+ * 确保只执行一次函数
+ *
+ * @export
+ * @param {Function} fn
+ * @returns Function
+ */
+export function once (fn:Function):Function {
+  let called = false
+  return function (...rest:[any]):void {
+    if (!called) {
+      called = true
+      fn.apply(this, rest)
+    }
+  }
+}
+
+/**
+ * 深度拷贝一个目标
+ *
+ * @export
+ * @param {any} target
+ * @returns any
+ */
+export function deepClone (target:object,source:object):object {
+  for (const key in source) {
+    const dataType = _toString.call(source[key])
+    if (dataType === '[object Object]') {
+        target[key] = {}
+        deepClone(target[key],source[key])
+    }else if(dataType === '[object Array]'){
+        target[key] = []
+        deepClone(target[key],source[key])
+    } else {
+        target[key] = source[key]
+    }
+  }
+  return target
+}
+
+/**
+ *  防抖函数，使一个函数在一定期间高频率下只执行一次的办法。
+ *
+ * @export
+ * @param {Function} event
+ * @param {Number} wait
+ * @param {Boolean} immediately
+ * @returns
+ */
+export function debounce (event:Function = noop, wait:number = 50, immediately:boolean = false):Function {
+  let timer:NodeJS.Timeout | null = null
+  let called:boolean = false
+
+  return function (...rest:[any]):void {
+    if (!timer) {
+      clearTimeout(timer)
+    }
+    if (immediately && !called) {
+      event.apply(this, [rest])
+      called = true
+    } else {
+      timer = setTimeout(() => {
+        event.apply(this, [rest])
+      }, wait)
+    }
+  }
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,6 +1,60 @@
-import index from '../src/index'
+/*
+ * @Author: pfzhengd
+ * @Date: 2020-06-29 17:59:04
+ * @LastEditors: pfzhengd
+ * @LastEditTime: 2020-06-30 12:08:54
+ * @Description: file content
+ */ 
+import {isPlainObject,hasOwn,extend,deepClone,once} from '../src/index'
+import {log} from '../src/debug'
 
-test("version is 0.0.1?", () => {
-    const test = index;
-    expect(test()).toBe("The current version is 0.0.1");
+test("isPlainObject ?", () => {
+    expect(isPlainObject([])).toBe(false)
+    expect(isPlainObject({})).toBe(true)
+    expect(isPlainObject({a:2})).toBe(true)
 });
+
+test("hasOwn ?", () => {
+    expect(hasOwn({a:1},'a')).toBe(true)
+    expect(hasOwn([1,2,3],'b')).toBe(false)
+    expect(hasOwn({},'__proto__')).toBe(false)
+});
+
+test('extend ?',()=>{
+    const result = extend({a:1},{b:2})
+    expect(hasOwn(result,'b')).toBe(true)
+    log('result',JSON.stringify(result))
+    expect(result['b']).toEqual(2)
+    expect(result['a']).toEqual(1)
+})
+
+test('deepClone ?',()=>{
+    const result = {} 
+    deepClone(result,{
+        b:2,
+        c:{
+            c1:'2.1',
+            c2:'2.2'
+        },
+        a:1,
+        e:[1,2,3,4]
+    })
+        
+    expect(hasOwn(result,'c')).toBe(true)
+    log('result',JSON.stringify(result))
+    expect(hasOwn(result['c'],'c1')).toBe(true)
+    expect(hasOwn(result['c'],'c2')).toBe(true)
+    expect(hasOwn(result['c'],'c2')).toBe(true)
+})
+
+test('once ?',()=>{
+    let counter = 0
+    const fn = once((a:number,b:number)=>{
+        log('我只执行了一次。')
+        counter += (a + b)
+    })
+    fn(1,2)
+    fn(1,2)
+    expect(counter).toBe(3)
+})
+
