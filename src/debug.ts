e1a44de2774de491b9cc6cb995784d7dcb1b927b
pfzheng <pengfeizheng@hotmail.com>
2021-05-12 12:10:55 +0800

    fix: 修复debug输出日志带undefined.

--- src/debug.ts
+++ src/debug.ts
@@ -9,32 +9,25 @@ export const config = {
  * @author pfzheng
  * @date 2021-05-07
  * @param {string} typeName 日志的类型
- * @param {string} name 错误标识名
- * @param {string} [msg] 错误信息
- * @param {...any} rest  错误附带信息
+ * @param {...any} rest  附带信息
  */
-function common(typeName:string,name:string,msg?:string,...rest:any){
+function common(typeName:string,rest:Array<any>){
   if (console) {
-    const args = Array.prototype.slice.call(arguments)
-    if(args.length === 0){
-      return
-    }
-    if (args.length === 1) {
-      msg = name
+    if (rest.length === 1) {
       console[typeName](
         `[${formatDate('hh:MM:ss', new Date())}][${config.moduleName}]`,
-        msg
+        rest[0]
       )
-    } else if (args.length === 2) {
+    } else if (rest.length === 2) {
       console[typeName](
-        `[${formatDate('hh:MM:ss', new Date())}][${config.moduleName}.${name}]`,
-        msg
+        `[${formatDate('hh:MM:ss', new Date())}][${config.moduleName}.${rest[0]}]`,
+        rest[1]
       )
     }else{
       console[typeName](
-        `[${formatDate('hh:MM:ss', new Date())}][${config.moduleName}.${name}]`,
-        msg,
-        ...rest
+        `[${formatDate('hh:MM:ss', new Date())}][${config.moduleName}.${rest[0]}]`,
+        rest[1],
+        ...rest.slice(2)
       )
     }
   }
@@ -42,27 +35,27 @@ function common(typeName:string,name:string,msg?:string,...rest:any){
 
 /**
  * @description 警告日志
- * @param {*} name
- * @param {*} msg
+ * @param {*} name 第一个参数
+ * @param {*} msg  第二个参数
  */
-export const warn = function (name: string, msg?: string,...rest:any) {
-  common('warn',name,msg,rest)
+export const warn = function (...rest:any) {
+  common('warn',rest)
 }
 
 /**
  * @description 错误日志
- * @param {*} name
- * @param {*} msg
+ * @param {*} name 第一个参数
+ * @param {*} msg  第二个参数
  */
-export const error = function (name: string, msg?: string,...rest:any) {
-  common('error',name,msg,rest)
+export const error = function (...rest:any) {
+  common('error',rest)
 }
 
 /**
  * @description 打印日志
- * @param {*} name
- * @param {*} msg
+ * @param {*} name 第一个参数
+ * @param {*} msg  第二个参数
  */
-export const log = function (name: string, msg?: string,...rest:any) {
-  common('log',name,msg,rest)
+export const log = function (...rest:any) {
+  common('log',rest)
 }
