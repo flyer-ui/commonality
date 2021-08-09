4dc2e80eff82bb5b714c11d8fa2ec67050aec37d
pfzheng <pengfeizheng@hotmail.com>
2021-08-09 20:30:46 +0800

    feat: 发布版本自动生成更新日志。

--- publish-pre.js
+++ publish-pre.js
@@ -6,7 +6,7 @@ const { exec } = require('child_process');
       console.error('生成版本日志出现了错误 ')
     } else {
       console.log('版本日志生成成功，正在提交日志文件。')
-      exec('git add . && git commit -m "chore: 提交生成的更新日志。"', (err) => {
+      exec('git add . && git commit -m "docs: 提交生成的更新日志。"', (err) => {
         if (err) {
           console.error('提交更新日志文件到 git 出现了错误。', err)
         } else {
